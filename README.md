54a5a1cf6c4d78062e5db8e7cbf41250a0c29457
Dodo33 <thedoodo33@gmail.com>
2017-08-07 17:08:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center"> Telegram ChatId Dumper </h1>
+
+
+Running this script you will get the chat_id of the telegram conversation with your bot. <br />
+Python3 required.
+
+
+`python3 chatiddumper.py <BotId>`
+
+Or, if your python path is */usr/bin/env python3*, just:
+
+`./chatiddumper.py <BotId>`
+
+The \<BotId\> is the token that BotFather give you when the bot is created.
+
+
+## Output example:
+
+`./chatiddumper.py XX:YYY-YYY` <br />
+<br />
+`botId: XX:YYY-YYY` <br />
+`HTTP response code: 200` <br />
+`ChatId: 372472551` <br />
---
+++ chatiddumper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import urllib.request
+import json
+from time import sleep
+
+def main(argv):
+    botId = argv[1]
+    url = "https://api.telegram.org/bot" + botId + "/getUpdates"
+
+    print("botId: " + botId)
+    
+    try:
+        response = urllib.request.urlopen(url)
+        print("HTTP response code: " + str(response.getcode()))
+    except Exception as e:
+        print("Exception " + str(e.__class__.__name__) + " " + str(e))
+        exit()
+
+    jsonData = json.loads(response.read().decode())
+    if len(jsonData["result"]) == 0:
+        print("I can't get the chatId if you don't send a message to the bot first. Try to send a random message and run me again.")
+        exit()
+    
+    chatId = jsonData["result"][0]["message"]["chat"]["id"]
+    print("ChatId: " + str(chatId))
+    
+if __name__ == "__main__":
+    main(sys.argv)
